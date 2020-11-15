ae39d540d7e65a1899080755eaf34732c115ec05
Jürgen <juergen.muelbert@gmail.com>
2020-11-15 09:58:35 +0100

    Upgrade to the newest Hypermodern Python version

--- docs/conf.py
+++ docs/conf.py
@@ -40,5 +40,11 @@ from datetime import datetime
 project = "jmopenorders"
 author = "Jürgen Mülbert"
 copyright = f"{datetime.now().year}, {author}"
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_click",
+    "sphinx_rtd_theme",
+]
 autodoc_typehints = "description"
+html_theme = "sphinx_rtd_theme"
